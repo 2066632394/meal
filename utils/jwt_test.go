70a9b8d26a9056ef1a03b7182eb393c0b98ddc08
szh <szh@disanbo.com>
2019-10-24 16:10:01 +0800

    update

--- utils/jwt_test.go
+++ utils/jwt_test.go
@@ -2,25 +2,12 @@ package utils
 
 import (
 	"testing"
-	"time"
 	"fmt"
 )
 
 func TestJwt(t *testing.T) {
-	var et EasyToken
-	et.Expires = time.Now().Add(time.Hour * 10).Unix()
-	et.Username = "test123"
-	token,err := et.GetToken()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Print("token",token)
-	check,err := et.ValidateToken(token)
-	if err != nil {
-		panic(err)
-	}
-	if check {
-		fmt.Println("PASS")
-	}
-
+	str := RandomString(6)
+	fmt.Println("STR",str)
 }
+
+
--- utils/stringtool.go
+++ utils/stringtool.go
@@ -33,7 +33,7 @@ func RandomString(length int) string {
 	rand.Seed(time.Now().UnixNano())
 	result := make([]string, 0, length)
 	for i := 0; i < length; i++ {
-		t := rand.Intn(62)
+		t := rand.Intn(10)
 		if t < 10 {
 			result = append(result, strconv.Itoa(rand.Intn(10)))
 		} else if t < 36 {
