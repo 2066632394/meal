2f90c3611338298d13e7b7e8a300bedeb68e9599
szh <szh@disanbo.com>
2019-10-24 23:17:36 +0800

    up

--- controllers/MealController.go
+++ controllers/MealController.go
@@ -223,14 +223,15 @@ func (c *MealController) UploadImage() {
 		if len(sp) != 2 {
 			c.jsonResult(enums.JRCodeFailed, "上传失败", "")
 		}
-		newfile := time.Unix(utils.GetNow(),0).Format("20060102")+utils.RandomString(10)+sp[1]
+		newfile := filepreix+time.Unix(utils.GetNow(),0).Format("2006-01-02")+utils.RandomString(10)+sp[1]
 		// 保存位置在 static/upload, 没有文件夹要先创建
 		err = go_image.ScaleF2F(filePath, newfile,600 )
 		if err != nil {
 			panic(err)
 		}
-
-		c.jsonResult(enums.JRCodeSucc, "上传成功", "/"+filePath)
+		logs.Info("oldfile",filePath)
+		logs.Info("newfile",newfile)
+		c.jsonResult(enums.JRCodeSucc, "上传成功", "/"+newfile)
 	} else {
 		c.jsonResult(enums.JRCodeFailed, "上传失败", "")
 	}
